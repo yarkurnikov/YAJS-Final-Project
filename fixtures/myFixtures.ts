f7ffc72c7121a28e3abe405ff9e05fa714635f49
Yaroslav Kurnykov <yaroslav.kurnykov@holywater.tech>
2025-08-25 18:30:55 +0300

    Unit 14 API fixture&Mock data

--- fixtures/myFixtures.ts
+++ fixtures/myFixtures.ts
@@ -1,5 +1,6 @@
 import { test as base } from '@playwright/test';
 import { AllPages } from '../pages/allPages';
+import { AUTH_CREDENTIALS } from '../constants/auth.constants';
 
 type MyFixtures = {
   allPages: AllPages;
@@ -12,10 +13,26 @@ export const test = base.extend<MyFixtures>({
     await use(allPages);
   },
 
-  loggedInApp: async ({ browser }, use) => {
-    const context = await browser.newContext({ storageState: './playwright/.auth/user.json' });
+  loggedInApp: async ({ browser, request }, use) => {
+    const response = await request.post('https://api.practicesoftwaretesting.com/users/login', {
+      data: {
+        'email': AUTH_CREDENTIALS.customer.email,
+        'password': AUTH_CREDENTIALS.customer.password,
+      }
+    });
+
+    const responseBody = await response.json() as { access_token: string };
+    const authToken = responseBody.access_token;
+
+    const context = await browser.newContext({
+      extraHTTPHeaders: {
+        'Authorization': `Bearer ${authToken}`
+      }
+    });
+
     const page = await context.newPage();
     const allPages = new AllPages(page);
+    
     await use(allPages);
     await context.close();
   }
