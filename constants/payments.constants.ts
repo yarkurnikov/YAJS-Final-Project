61b8af72fcdaa68dc8eca833d10f4cbba7b6a99b
Yaroslav Kurnykov <yaroslav.kurnykov@holywater.tech>
2025-09-07 14:56:51 +0300

    fixes after review

--- constants/payments.constants.ts
+++ constants/payments.constants.ts
@@ -1,8 +1,9 @@
 import { addMonths } from '../helpers/addMonths.helper';
+import { CREDIT_CARD_NUMBER, CREDIT_CARD_CVV, CREDIT_CARD_HOLDER_NAME } from '../config/baseConfig';
 
 export const CREDIT_CARD_DATA = {
-  creditCardNumber: process.env.CREDIT_CARD_NUMBER!,
+  creditCardNumber: CREDIT_CARD_NUMBER,
   creditCardExpirationDate: addMonths(Date.now(), 3),
-  creditCardCvv: process.env.CREDIT_CARD_CVV!,
-  creditCardHolderName: process.env.CREDIT_CARD_HOLDER_NAME || 'Test User'
+  creditCardCvv: CREDIT_CARD_CVV,
+  creditCardHolderName: CREDIT_CARD_HOLDER_NAME
 };
--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,5 @@
 import { defineConfig, devices } from '@playwright/test';
-
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-import dotenv from 'dotenv';
-import path from 'path';
-dotenv.config({ path: path.resolve(__dirname, '.env') });
+import { BASE_URL } from './config/baseConfig';
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -29,7 +22,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.BASE_URL,
+    baseURL: BASE_URL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
